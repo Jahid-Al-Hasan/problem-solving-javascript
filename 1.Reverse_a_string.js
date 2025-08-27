39771e2fa0bb2eb4456d4ea606ea19bec22b09f2
Jahid <hzahid905@gmail.com>
2025-08-27 13:55:42 +0600

    reverse a string

---
+++ 1.Reverse_a_string.js
@@ -0,0 +1,9 @@
+function reverse(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+
+console.log(reverse("hello"));
