4f262ecfeaac68e451a059016198ddc5840f486f
Jahid <hzahid905@gmail.com>
2025-08-28 03:19:07 +0600

    check for palindrome

---
+++ 3.Check_palindrome.js
@@ -0,0 +1,30 @@
+/*
+Problem 3: Check for Palindrome
+Write a function that checks if a string is a palindrome (reads the same forward and backward).
+
+Example:
+
+Input: "madam"
+Output: true
+
+Input: "hello"
+Output: false
+*/
+
+// Solution:-
+function palindrome(str) {
+  let pointer1 = 0;
+  let pointer2 = str.length - 1;
+
+  while (pointer1 < pointer2) {
+    if (str[pointer1] !== str[pointer2]) {
+      return false;
+    }
+    pointer1++;
+    pointer2--;
+  }
+  return true;
+}
+
+console.log(palindrome("madam"));
+console.log(palindrome("hello"));
